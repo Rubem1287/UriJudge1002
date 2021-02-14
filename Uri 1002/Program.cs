55fb450d9dea226cbbfd93ae1a8d48ea104e6ff2
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:18:13 -0300

    Add files via upload

---
+++ Uri 1002/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, radio, n,r;
+
+            n = 3.14159;
+            
+            
+
+            Console.WriteLine();
+            Console.Write("Digite o valor do raio : ");
+            r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            radio = Math.Pow(r, 2);
+
+            area = radio * n;
+
+            Console.WriteLine();
+            Console.Write("A = "+area.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
